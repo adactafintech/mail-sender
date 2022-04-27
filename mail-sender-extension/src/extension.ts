e2cf2b8b5f4c3a79b03800422582828a4398bafd
Nenad Maricic <nenad.maricic@adacta-fintech.com>
2022-04-27 11:41:15 +0200

    command added

--- mail-sender-extension/src/extension.ts
+++ mail-sender-extension/src/extension.ts
@@ -1,5 +1,12 @@
 import * as vscode from 'vscode';
+import { MailSenderCommands } from './model/commands';
 
 export function activate(context: vscode.ExtensionContext) {
-    vscode.window.showInformationMessage('Mail Sender Activated');
+	vscode.window.showInformationMessage('Mail Sender Activated');
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand(MailSenderCommands.Show, () => {
+            vscode.window.showInformationMessage('Mail Sender Should Show.');
+        })
+    );
 }
---
+++ mail-sender-extension/src/model/commands.ts
@@ -0,0 +1,3 @@
+export enum MailSenderCommands {
+    Show = 'mail-sender.show',
+}
